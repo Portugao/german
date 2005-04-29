69283780ef6043c80130cacb0fc68d3ca41be83f
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2005-04-29 17:15:23 +0000

    initial import

---
+++ sprachpaket/html/modules/Languages/lang/deu/global.php
@@ -0,0 +1,64 @@
+<?php
+// Generated: $d$ by $id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2001 by The PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Original Author of file: Andreas Krapohl <larsneo@postnuke.com>
+// Purpose of file: Translation files
+// Translation team: Read credits in /docs/CREDITS.txt
+// ----------------------------------------------------------------------
+define('_ACTMULTILINGUAL','ML aktivieren');
+define('_ACTUSEFLAGS','Flaggen aktivieren');
+define('_ACTAUTODETECT', 'Sprache aufgrund der Browser-Einstellung voreinstellen');
+define('_ADDMISSING','Alle fehlenden Übersetzungen sind in der Datei missing.php im language-Verzeichnis,');
+define('_ADMINTRANSLATION','Dateiliste');
+define('_CLICKLINK','Bitte auf den Link klicken');
+define('_CONSTANTSLOADING','Alle Konstanten geladen,');
+define('_DIEOPEN','konnte nicht öffnen');
+define('_GENERATELANG','erzeugen');
+define('_GENERATELANG1','Datei erzeugen');
+define('_GENERATELANG2','Alle Dateien für die gewählte Sprache');
+define('_GENERATELANG3','befinden sich im Verzeichnis <strong>language_');
+define('_GENERATELANG4','Zur Distribution müssen alle Dateien in das <strong>root</strong> Verzeichnis geschrieben werden.');
+define('_LANGCONF','Sprach-Konfiguration');
+define('_LANGUAGE_ALL_CONSTANT_HARDCODE','Alle Konstanten waren hardcoded');
+define('_LANG_ADD_MISSING','Fehlende hinzufügen');
+define('_LANG_CURENT','Derzeitige Sprache');
+define('_LANG_DEFAULT_TRANSLATION','Voreinstellung');
+define('_LANG_FILE','Sprachdatei');
+define('_LANG_FILE_TARGET','Zieldatei');
+define('_LANG_GENERATE','Erzeugen');
+define('_LANG_HARDCODE','Hardkodiert');
+define('_LANG_LANGUAGES_NUMBER','Anzahl der Sprachdefinitionen');
+define('_LANG_LOAD_CONSTANT','Laden der Konstanten');
+define('_LANG_LOAD_TRANSLATION','Übersetzung laden');
+define('_LANG_MISSING_NUMBER','Fehlende Zahl');
+define('_LANG_NUM_CONSTANT','Konstanten Zahl');
+define('_LANG_NUM_TRANSLATION','Übersetzung Nummer');
+define('_LANG_OBJECTIVE','Standard');
+define('_LANG_RELOAD_CONSTANT','Konstanten neu laden');
+define('_LANG_SELECT','Sprache wählen');
+define('_NEXT_OP_TODO','Nächster Schritt');
+define('_TARGET','Erzeugen');
+define('_TRANSLATIONSLOADING','Alle Übersetzungen geladen für');
+?>
