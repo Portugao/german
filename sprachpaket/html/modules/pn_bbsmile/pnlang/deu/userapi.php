04c5377fce1d82407864f859eb193c3a8e943f55
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2005-08-08 09:02:56 +0000

    sync with noc project

--- sprachpaket/html/modules/pn_bbsmile/pnlang/deu/userapi.php
+++ sprachpaket/html/modules/pn_bbsmile/pnlang/deu/userapi.php
@@ -1,4 +1,5 @@
 <?php
+// $Id$
 // ----------------------------------------------------------------------
 // PostNuke Content Management System
 // Copyright (C) 2001 by the PostNuke Development Team.
@@ -26,5 +27,13 @@
 // changed to pn_bbsmile: larsneo
 // ----------------------------------------------------------------------
 
-define('_PNBBSMILE_ARGSERROR',                 '[pn_bbsmile] Internal error! Arguments missing!');
+// include smilie alternative text defines
+@include_once('modules/pn_bbsmile/pnlang/deu/smilies.php');
+
+define('_PNBBSMILE_NOSCRIPTWARNING',           'Ihr Browser unterstützt kein Javascript oder Javascript ist deaktiviert. Einige Features dieser Seite werden daher nicht funktionieren.');
+define('_PNBBSMILE_ARGSERROR',                 '[pn_bbsmile] Interner Fehler! Parameter fehlen!');
+define('_PNBBSMILE_MORESMILIES',               'Weitere Smilies');
+
+define('_PNBBSMILE_SHOWHIDE_SMILIES','Smilies ein-/ausblenden');
+
 ?>
