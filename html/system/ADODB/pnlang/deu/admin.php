66e3bbb78692a9d2c4a34a6735c4f2d929cd4628
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2006-12-03 11:07:11 +0000

    initial commit [larsneo]

---
+++ html/system/ADODB/pnlang/deu/admin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2001, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 18167 2006-03-16 01:49:56Z drak $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package     PostNuke_System_Modules
+ * @subpackage  PostNuke_ADODB
+*/
+
+define('_ADODB', 'PostNuke ADODB');
+define('_ADODBFLUSHCACHE', 'Cache löschen');
+define('_ADODBCACHEBLOCKS', 'Dauer (Sekunden) für das Cache der Block Information');
+define('_ADODBQUERYCACHEINTERVAL', 'Dauer (Sekunden) für das Query-Caching Intervall');
+define('_ADODBCONFIRMFLUSHCACHE', 'Löschen des Cache bestätigen');
+define('_ADODBCACHEFLUSHED', 'Cache gelöscht');
+define('_ADODBCACHEDIRECTORY', 'Cache Verzeichnis');
+define('_ADODBDATABASETYPE', 'DB Typ');
+define('_ADODBDATAPROVIDER', 'DB Provider');
+define('_ADODBDATABASEHOST', 'DB Host');
+define('_ADODBDATABASENAME', 'DB Name');
+define('_ADODBVERSION', 'ADODB Version');
+define('_ADODBDATABASEDESCRIPTION', 'DB-Server Beschreibung');
+define('_ADODBDATABASEVERSION', 'DB-Server Version');
+
+?>
