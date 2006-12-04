3dab5ae9858418d5aa4344f861575b7349543934
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2006-12-04 21:51:15 +0000

    german translation [larsneo]

--- sprachpaket_8/html/modules/blank/pnlang/deu/admin.php
+++ sprachpaket_8/html/modules/blank/pnlang/deu/admin.php
@@ -8,8 +8,8 @@
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 
-define('_BLANKMOD_ADMIN_TITLE',         'Blank Module Administration Title');
-define('_BLANKMOD_ADMIN_CONTENT',       'Blank Module Administration Content');
-define('_BLANKMOD_ADMIN_CONTENT_TITLE', 'Blank Module Content Title');
+define('_BLANKMOD_ADMIN_TITLE',         'Blank Module Administration Titel');
+define('_BLANKMOD_ADMIN_CONTENT',       'Blank Module Administration Inhalt');
+define('_BLANKMOD_ADMIN_CONTENT_TITLE', 'Blank Module Inhalt Titel');
 
 ?>
