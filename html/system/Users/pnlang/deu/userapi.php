9203e84e3e91f797e1ec35f204491f9d3240943e
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2006-12-09 13:59:37 +0000

    update

--- html/system/Users/pnlang/deu/userapi.php
+++ html/system/Users/pnlang/deu/userapi.php
@@ -12,7 +12,7 @@
 */
 
 define('_AT','um');
-define('_ACCOUNTPENDING', 'Die Anmeldung muß durch den Seitenbetreiber noch freigeschaltet werden. Nach der Freigabe erfolgt eine E-Mail-Benachrichtigung');
+define('_ACCOUNTPENDING', 'Die Anmeldung muß noch durch den Seitenbetreiber freigeschaltet werden, nach der Freigabe erfolgt eine E-Mail-Benachrichtigung.');
 define('_AWEBUSERFROM','Jemand mit der IP-Adresse');
 define('_CODEREQUESTED','hat eine Bestätigung angefordert um das Kennwort zu ändern.');
 define('_FOLLOWINGMEM','Die gespeicherte Information ist:');
@@ -20,18 +20,18 @@ define('_HASREQUESTED','hat einen Kennwort-Versand angefordert.');
 define('_HASTHISEMAIL','hat diese E-Mail-Adresse hinterlegt.');
 define('_IFYOUDIDNOTASK','Falls keine entsprechende Anforderung gestellt wurde kann diese Benachrichtigung ignoriert werden.');
 define('_IFYOUDIDNOTASK2','Falls keine entsprechende Anforderung gestellt wurde kann diese Benachrichtigung gelöscht werden.');
-define('_KEEPTHISEMAILSAFE', 'Bitte diese E-Mail speichern, es ist die einzige Kennwort-Benachrichtigung.');
+define('_KEEPTHISEMAILSAFE', 'Bitte diese E-Mail speichern, es ist die einzige Kennwort-Benachrichtigung!');
 define('_NOTIFYEMAILCONT1','Ein neuer Benutzer hat sich registriert. Benutzername: ');
 define('_NOTIFYEMAILCONT2','');
 define('_NOTIFYEMAILSUB','Benutzerregistrierung');
-define('_TOREGISTER','um einen Account anzulegen bei');
+define('_TOREGISTER','hat ein Benutzerkonto eingerichtet bei');
 define('_UNICKNAME','Benutzername:');
 define('_UPASSWORD','Kennwort:');
 define('_USERACCOUNT','Der Benutzer-Account');
 define('_USERPASS4','Kennwort für');
 define('_USERPASS42',''); //Add for non eng translation
 define('_USERPASSWORD4','Benutzer-Kennwort für');
-define('_USERAPPROVED', 'Die Anmeldung wurde freigschaltet. Das Kennwort wurde bereits per E-Mail versendet.');
+define('_USERAPPROVED', 'Die Anmeldung wurde jetzt freigeschaltet. Das Kennwort wurde bereits per E-Mail versendet.');
 define('_USERSACTIVATIONMAIL', 'Aktivierung von');
 define('_USERSACIVATEYOURACCOUNTLINK', 'Bitte den folgenden Link anklicken um die Registrierung abzuschliessen');
 define('_USERSONCEACTIVATED', 'Nach der Freischaltung sind die Account-Daten');
@@ -42,5 +42,5 @@ define('_WITHTHISCODE','Mit diesem Bestätigungs-Code kann ein neues Kennwort ang
 define('_YOUCANCHANGE','Nach der Anmeldung kann es geändert werden');
 define('_YOURCODEIS','Der Bestätigungs-Code ist:');
 define('_YOURNEWPASSWORD','Das neue Kennwort ist:');
-define('_YOUUSEDEMAIL','Die folgende E-Mail-Adresse wurde genutzt');
+define('_YOUUSEDEMAIL','Jemand mit der E-Mail-Adresse');
 ?>
