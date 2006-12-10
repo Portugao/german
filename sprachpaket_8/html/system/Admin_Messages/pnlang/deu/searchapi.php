20f6b7c95fe63618e6339d232db7b8e35d9e39ab
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2006-12-10 20:52:53 +0000

    initial commit

---
+++ sprachpaket_8/html/system/Admin_Messages/pnlang/deu/searchapi.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2001, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: searchapi.php 18167 2006-03-16 01:49:56Z drak $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package     PostNuke_System_Modules
+ * @subpackage  PostNuke_AdminMessages
+*/
+
+define('_ADMINMESSAGESSEARCH', 'Admin-Nachrichten');
+define('_ADMINMESSAGESACTIVEONLY', 'nur aktive');
+define('_ADMINMESSAGESSEARCHRESULTS', 'Admin-Nachricht(en) gefunden');
+define('_ADMINMESSAGESSEACHNONEFOUND', 'keine Admin-Nachrichten gefunden');
+
+?>
