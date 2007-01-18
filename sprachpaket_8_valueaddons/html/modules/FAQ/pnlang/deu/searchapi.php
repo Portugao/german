1c893cc9add654f1ff35fd9d36cbd5f3fd414830
perle22121979 <perle22121979@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-01-18 00:09:09 +0000

    2 more Modules translated

---
+++ sprachpaket_8_valueaddons/html/modules/FAQ/pnlang/deu/searchapi.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: searchapi.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage FAQ
+ */
+
+define('_FAQ_CATEGORIES', 'FAQs');
+define('_FAQ_SEARCH', 'FAQ Suche');
+define('_FAQ_SEARCHRESULTS', 'FAQs gefunden');
+define('_FAQ_SEACHNONEFOUND', 'Keine FAQs gefunden');
+
+?>
---
+++ sprachpaket_8_valueaddons/html/modules/FAQ/pnlang/deu/user.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: user.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage FAQ
+ */
+
+define('_FAQ_',            'Frequently Asked Questions');
+define('_FAQ_ANSWER',      'Antwort');
+define('_FAQ_ITEMFAILED',  'Objekte anzeigen fehlgeschlagen');
+define('_FAQ_QUESTION',    'Frage');
+define('_FAQ_VIEW',        'FAQ\'s ansehen');
+define('_FAQ_SUBMITTEDBY', 'Eingesandt von');
+define('_FAQ_ANSWEREDBY',  'Beantworted von');
+define('_FAQ_ASK',         'Eine Frage stellen');
+define('_FAQ_YOURNAME',    'Name');
+define('_FAQ_ASKDETAIL',   'Vorm stellen einer Frage bitte überprüfen ob die Frage schon in den FAQ\'s vorhanden ist.
+ Wenn nicht dann die Frage mit dem unten stehenden Formular. Die Frage sollte klar und deutlich formuliert werden. 
+Nach der Übermittlung der Frage wird sie von den Redakteuren der Seite überprüft.');
+define('_FAQ_CREATED',     'Danke für die Frage');
+
+?>
