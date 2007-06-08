8df62fbacc3c2ba9d03c9a14b27d7e27e43195ea
t_smiatek <t_smiatek@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-06-08 11:24:22 +0000

    upupdate to latest core svn state

---
+++ html/system/Categories/pnlang/deu/init.php
@@ -0,0 +1,27 @@
+<?php
+
+define('_CATDISP_MODULES', 'Module');
+define('_CATDISP_GENERAL', 'Allgemein');
+define('_CATDISP_YESNO', 'Ja/Nein');
+define('_CATDISP_DEFAULTENTRIES', 'Standardeinträge');
+define('_CATDISP_DEFAULTVALUE', 'Dies ist ein Standardwert');
+define('_CATDISP_CUSTOMIZE', 'Dies muß weiter angepasst werden!');
+define('_CATDISP_PENDINGSTATUS', 'Warte-Status');
+define('_CATDISP_PENDING', 'Wartend');
+define('_CATDISP_CHECKED', 'Markiert');
+define('_CATDISP_APPROVED', 'Akzeptiert');
+define('_CATDISP_ONLINE', 'Online');
+define('_CATDISP_REJECTED', 'Abgelehnt');
+define('_CATDISP_GENDER', 'Geschlecht');
+define('_CATDISP_MALE', 'Mann');
+define('_CATDISP_FEMALE', 'Frau');
+define('_CATDISP_TITLE', 'Titel');
+define('_CATDISP_DR', 'Dr.');
+define('_CATDISP_PROF', 'Prof.');
+define('_CATDISP_MAG', 'Mag.');
+define('_CATDISP_ACTIVITYSTATUS', 'Aktivitätsstatus');
+define('_CATDISP_ACTIVE', 'Aktiv');
+define('_CATDISP_INACTIVE', 'Inaktiv');
+define('_CATDISP_PENDINGSTATUSBASIC', 'Basis des Warte-Status');
+
+?>
