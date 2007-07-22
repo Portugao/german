f7b317a1eeddabf5b0b284db796e6bdb83772f84
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-07-22 12:37:13 +0000

    added Multisite - not yet perfect

---
+++ sprachpaket_8_valueaddons/html/modules/Multisites/pnlang/deu/init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 20347 2006-10-19 15:59:36Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Multisites
+ */
+
+define('_MULTISITES_MASTERNAME', 'Hauptseite');
+define('_MULTISITES_MASTERDESCRIPTION', 'Hauptseite - any shared tables will be based on this site; This site cannot be deleted.');
