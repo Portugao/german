6a3cb15abed2d7e2c560c48adbad1b8e9022b92e
wms <wms@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-08-15 17:36:53 +0000

    Thumbnail neu hinzugefügt

---
+++ sprachpaket_8_valueaddons/html/modules/Thumbnail/pnlang/deu/common.php
@@ -0,0 +1,11 @@
+<?php 
+define('_V4B_THUMBNAIL_UPDATE',					'Speichern');
+define('_V4B_THUMBNAIL_CANCEL',					'Abbrechen');
+
+define('_V4B_THUMBNAIL_TITLE_PREFERENCES',		'Thumbnail Modul Einstellungen');
+define('_V4B_THUMBNAIL_PREFERENCES_SIZE',		'Standard Thumbnail Grösse');
+define('_V4B_THUMBNAIL_PREFERENCES_OVERRIDE',	'Aufrufendes Modul darf alternative Grösse verlangen');
+
+define('_V4B_THUMBNAIL_TITLE_SETTINGS',			'phpThumb Einstellungen');
+
+define('_V4B_THUMBNAIL_TITLE_CHECKSETTINGS',	'Einstellungen überprüfen');
