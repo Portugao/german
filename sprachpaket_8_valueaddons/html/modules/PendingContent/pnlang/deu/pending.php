cbbc4b797863dac81c330079f56d65f21e450631
wms <wms@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-08-15 17:33:14 +0000

    PendingContent neu hinzugefügt.

---
+++ sprachpaket_8_valueaddons/html/modules/PendingContent/pnlang/deu/pending.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: pending.php 22139 2007-06-01 10:57:16Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage PendingContent
+ */
+
+define('_PENDINGGOTOADMIN', '[admin]');
