af5558e09fab2c8e81696b1143006539e1915d6c
wms <wms@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-08-15 17:38:43 +0000

    Sniffer neu hinzugefügt

---
+++ sprachpaket_8_valueaddons/html/modules/Sniffer/pnlang/deu/admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 22139 2007-06-01 10:57:16Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Sniffer
+ */
+
+define('_SNIFFER', 'Browser Sniffer');
+define('_SNIFFERBASICINFO', 'Basisinformationen zum Browser');
+define('_SNIFFERBROWSERNAME', 'Browser');
+define('_SNIFFERBROWSERVERSION', 'Browser version');
+define('_SNIFFEROSNAME', 'Betriebssystem');
+define('_SNIFFEROSVERSION', 'Version des Betriebssystems');
+define('_SNIFFERUA', 'User Agent');
