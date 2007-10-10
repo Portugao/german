a568c74dd777e6f54d99bd633d5b0324514175c3
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-10-10 17:47:38 +0000

    neueste Änderungen von Robert berücksichtigt. Bitte prüfen.

--- html/system/Categories/pnlang/deu/common.php
+++ html/system/Categories/pnlang/deu/common.php
@@ -21,19 +21,34 @@ define('_CATEGORIES_REGISTRY',                'Kategoriezuweisung bearbeiten');
 // view template
 define('_CATEGORIES_VIEW_TITLE',              'Alle Kategorien');
 
+// user template
+define('_CATEGORIES_EDIT_SUBCATEGORIES',    	'Bearbeite Unterkategorien ab diesen Knoten');
+define('_CATEGORIES_EDIT_USER',			          'Benutzerkategorien bearbeiten');
+define('_CATEGORIES_EDIT_USER_DISABLED',     	'Die Bearbeitung von Benutzerkategorien ist deaktiviert. Der Administrator kann diese Funktion aktivieren.');
+define('_CATEGORIES_EDIT_USER_NOT_OWN',	    	'Die Bearbeitung von fremden Benutzerkategorien ist nicht möglich. Diese Funktion kann nur vom Administrator ausgeführt werden.');
+
 // new/edit template
 define('_CATEGORIES_ADDITIONALDATA',          'Zusätzliche Datenfelder');
 define('_CATEGORIES_ADVANCED',                'Umschalten zum erwiterten Modus');
 define('_CATEGORIES_BASEINFO',                'Basisinformation zur Kategorie');
-define('_CATEGORIES_DATA',                    'Datenfeld');
-define('_CATEGORIES_DOMAIN',                  'Domain');
+define('_CATEGORIES_CATEGORY_IS_LOCKED',    	'Diese Kategorie ist gesperrt und kann nur vom Administrator bearbeitet werden.');
+define('_CATEGORIES_IS_LEAF',		             	'Die Kategorie ist eine Unterkategorie');
+define('_CATEGORIES_IS_LOCKED',		           	'Die Kategorie ist gesperrt');
 define('_CATEGORIES_LOCALISATION',            'Lokalisierte Ausgabe');
 define('_CATEGORIES_META',                    'Metadaten der Kategorie');
 define('_CATEGORIES_PARENT',                  'Parent');
-define('_CATEGORIES_SECURITY_DOMAIN',         'Sicherheits-Domain');
-define('_CATEGORIES_SORT_VALUE',              'Sortier-Wert');
+define('_CATEGORIES_ROOT_PARENT_LOCKED',	    'Die Stammkategorie eines Startknoten kann nicht geändert werden.');
+define('_CATEGORIES_SECURITY_DOMAIN',         'Sicherheitsdomain');
+define('_CATEGORIES_SORT_VALUE',              'Sortierwert');
 define('_CATEGORIES_STATUS',                  'Aktiv');
 define('_CATEGORIES_VALUE',                   'Wert');
+
+// preferences template
+define('_CATEGORIES_PREFS',		               	'Einstellungen');
+define('_CATEGORIES_PREFS_USERROOTCAT',		    'Stammkategorie für Benutzerkategorien');
+define('_CATEGORIES_PREFS_ALLOWUSERCATEDIT',	'Benutzer erlauben, die eigenen Kategorien bearbeiten zu dürfen');
+define('_CATEGORIES_PREFS_AUTOCREATEUSERCAT',	'Automatisches Erstellen des Stammverzeichnis für Benutzerkategorien');
+define('_CATEGORIES_REBUILDPATHS', 	        	'Pfade neu erstellen');
 
 // list template
 define('_CATEGORIES_RESEQUENCE',              'Index erneuern');
@@ -57,6 +72,7 @@ define('_CATEGORIES_COPY_TITLE',              'Kategorie kopieren');
 
 // modify config template
 define('_CATEGORIES_CONFIG_REBUILD',          'Hier können alle systeminternen Pfade neu erstellt werden');
+define('_CATEGORIES_CONFIG_REBUILD_INFO', 	  '<br /><br />Sie können die Aktion auslösen, wenn Sie auf den Submit-Button klicken. Je nach Anzahl der Kategorien könnte ein Time-Out auftreten oder das PHP-Speicherlimit überschritten werden.');
 define('_CATEGORIES_CONFIG_REBUILD_PATHS',    'Pfade neu erstellen');
 
 // error/status messages
@@ -65,7 +81,13 @@ define('_CATEGORIES_DOCROOT_INVALID',         'Ungültiger docroot Parameter im U
 define('_CATEGORIES_EDITCAT_INVALID',         'Ungültige Kategorie kann nicht editiert werden');
 define('_CATEGORIES_EDITCAT_NOT_VALUEPARENT', 'Angegebene Kategorie ist keine übergeordnete Werte-Kategorie');
 define('_CATEGORIES_EDITCAT_NOT_SUBCAT',      'Angegebene Kategorie ist keine Tochter-Kateogorie von docroot');
-define('_CATEGORIES_ROOT_CANT_EDIT',          'Das root Verzeichnis kann nicht im User-Modus editiert werden');
 define('_CATEGORIES_NAME_EMPTY',              'Name darf nicht leer sein');
+define('_CATEGORIES_NO_USERCATNAME',		      'Fehler! Der Name des Stammknoten für die Benutzerkategorie konnte nicht ermittelt werden');
+define('_CATEGORIES_ROOT_CANT_EDIT',          'Das root Verzeichnis kann nicht im User-Modus editiert werden');
+define('_CATEGORIES_USERCAT_NOAUTOCREATE',  	'Fehler! Der Benutzerstammknoten für diesen Benutzer existiert nicht und das automatische Erstellung ist nicht gesetzt.');
+define('_CATEGORIES_USERCAT_LOGGED_IN',	    	'Fehler! Das Bearbeiten der Benutzerkategorien ist nur für angemeldete Benutzer möglich');
+define('_CATEGORIES_USERDOCROOT_EMPTY',	    	'Fehler! Der Benutzerstammknoten konnte nicht ermittelt werden');
+define('_CATEGORIES_USERDOCROOT_INVALID',   	'Fehler! Der Benutzerstammknoten scheint auf eine ungültige Kategorie zu verweisen');
 define('_CATEGORIES_VALUE_EMPTY',             'Wert darf nicht leer sein');
+
 
