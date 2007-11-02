3e1f310020ee342b47e0d1396ff571d42fd84835
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-02 23:59:16 +0000

    moved ValueAddons back again to /modules/

---
+++ html/modules/Members_List/pnlang/deu/featureduser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: membersonline.php 21541 2007-02-26 21:25:39Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Members_List
+ */
+
+// Ported from : i-Block [Featured User 1.5] - Alexander Graef aka MagicX - http://www.portalzine.de 
+
+define('_MEMBERSLIST_FEATUREDUSER_BLOCKNAME', 'Besondere Benutzer');
+define('_MEMBERSLIST_FEATUREDUSER_BLOCKDESC', 'Zeige besondere Benutzer');
+
+define('_MEMBERSLIST_REGISTEREDONDATE', 'Registriert am %date%');
---
+++ html/modules/Members_List/pnlang/deu/membersonline.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: membersonline.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Members_List
+ */
+
+// Ported from : i-Block [Members Online 2.5] - MagicX - Portalzine.de
+
+define('_MEMBERSLIST_MEMBERSONLINE_BLOCKNAME',    'Mitglieder Online');
+define('_MEMBERSLIST_MEMBERSONLINE_BLOCKDESC',    'Mitglieder Online anzeigen');
+
+define('_MEMBERSLIST_MAXCHARS',     'Anzahl Buchstaben max.');
+
+define('_MEMBERSLIST_ONLINECOUNT1', 'Es sind %a% unregistrierte Benutzer und %u% registrierte Benutzer online.');
+define('_MEMBERSLIST_ONLINECOUNT2', 'Es sind %a% unregistrierter Benutzer und %u% registrierter Benutzer online.');
+define('_MEMBERSLIST_ONLINECOUNT3', 'Es sind %a% unregistrierter Benutzer und %u% registrierte Benutzer online.');
+define('_MEMBERSLIST_ONLINECOUNT4', 'Es sind %a% unregistrierte Benutzer und %u% registrierter Benutzer online.');
+define('_MEMBERSLIST_ONLINECOUNT5', 'Es ist %u% registrierter Benutzer online.');
+define('_MEMBERSLIST_ONLINECOUNT6', 'Es sind %u% registrierte Benutzer online.');
