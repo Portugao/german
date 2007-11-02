ae040c1e9188d73cc4f5d4e01befec6703cbf063
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-02 23:57:26 +0000

    moved ValueAddons back again to /modules/

---
+++ sprachpaket_8/html/modules/Ephemerids/pnlang/deu/admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Ephemerids
+ */
+
+define('_EPHEMERIDS', 'Ephemeriden');
+define('_EPHEMERIDS_EPHEMERIDS', 'Ephemeriden');
+define('_EPHEMERIDS_EPHEMERID', 'Ephemerid');
+
+// view template
+define('_EPHEMERIDS_EVENT' ,'Beschreibung');
---
+++ sprachpaket_8/html/modules/FAQ/pnlang/deu/version.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2004, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: version.php 18301 2006-03-23 10:08:10Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage FAQ
+ */
+
+define('_FAQ_NAME',        'FAQ');
+define('_FAQ_DISPLAYNAME', 'FAQ');
+define('_FAQ_DESCRIPTION', 'Häufig gestellte Fragen');
