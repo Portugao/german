1faa5d4b58e6c6615bbf9194f450cad19a1d0db1
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-02 23:59:16 +0000

    moved ValueAddons back again to /modules/

---
+++ sprachpaket_8/html/modules/Members_List/pnlang/deu/admin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Members_List
+ */
+
+define('_MEMBERSLIST_MEMBERSPERPAGE', 'Objekte pro Seite auf der Members List Seite');
+define('_MEMBERSLIST_ONLINEMEMBERSITEMSPERPAGE', 'Objekte pro Seite auf der Members Online Seite');
+define('_MEMBERSLIST_RECENTMEMBERSITEMSPERPAGE', 'Objekte pro Seite auf der Members Letzte Benutzer Seite');
+define('_MEMBERSLIST_UNVERIFIED', 'unregistrierte Benutzer von der Members List filtern');
---
+++ sprachpaket_8/html/modules/Members_List/pnlang/deu/common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: user.php 21096 2007-01-17 16:58:37Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Members_List
+ */
+
+// general
+define('_MEMBERSLIST','Mitgliederliste');
