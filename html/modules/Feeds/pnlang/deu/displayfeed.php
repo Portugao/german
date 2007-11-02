6420016093c9204d5a73cc023b23d517f98727e9
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-02 23:58:21 +0000

    moved ValueAddons back again to /modules/

---
+++ html/modules/Feeds/pnlang/deu/displayfeed.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: displayfeed.php 21106 2007-01-18 08:14:17Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+  * @subpackage Feeds
+ */
+
+define('_FEEDS_NUMITEMS', 'Anzahl der Feeds zum Anzeigen (bis zum bereitgestellten Maximum des Feeds oder -1 für alle Artikel)');
+define('_FEEDS_FEEDID', 'Feed');
+define('_FEEDS_DISPLAYIMAGE', 'Feed Bild anzeigen (wenn im Feed verfügbar)');
---
+++ html/modules/HitCount/pnlang/deu/user.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: user.php 19262 2006-06-12 14:45:18Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage HitCount
+ */
+
+define('_HITCOUNT', 'Hit Counter');
+define('_HITCOUNTVIEW', 'Hit Count Übersicht');
+define('_HITCOUNTMODSWERECOUNTING', 'Module für die im Moment Hits gezählt werden');
+define('_HITCOUNTHITS', 'Hits');
+define('_HITCOUNTITEM', 'Objekt');
+define('_HITCOUNTITEMS', 'Objekte');
