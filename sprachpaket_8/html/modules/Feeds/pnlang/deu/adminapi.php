bbea12ff67a808e1ec835be183860e993552dd9a
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-02 23:58:21 +0000

    moved ValueAddons back again to /modules/

---
+++ sprachpaket_8/html/modules/Feeds/pnlang/deu/adminapi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: adminapi.php 21106 2007-01-18 08:14:17Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Feeds
+ */
+
+define('_FEEDS_INVALIDPROTOCOL', 'Falsches Protokoll - nur http und https sind möglich.');
+define('_FEEDS_URLTOOLONG', 'Die angegebene URL ist zu lang (max. 200 Zeichen).');
---
+++ sprachpaket_8/html/modules/Feeds/pnlang/deu/user.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: user.php 21111 2007-01-18 12:10:42Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Feeds
+ */
+
+define('_FEEDS_AVAILABLEFEEDS', 'Verfügbare Feeds');
+define('_FEEDS_BYAUTHOR', 'von %author%');
+define('_FEEDS_VIEW','Feeds anzeigen');
+define('_FEEDS_READMORE', 'mehr lesen....');
