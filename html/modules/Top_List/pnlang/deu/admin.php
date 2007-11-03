34d772a6803a0c8543b8b5d7b5c75eaeca9be9cd
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-03 00:00:50 +0000

    moved ValueAddons back again to /modules/

---
+++ html/modules/Top_List/pnlang/deu/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PostNuke Application Framework
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 22302 2007-07-02 16:24:27Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Top_List
+ */
+
+define('_ITEMSTOP','Einträge auf der Startseite');
+define('_TOPLIST', 'Top Liste');
