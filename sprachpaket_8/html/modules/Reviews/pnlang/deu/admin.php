86693b69d00449a3c911ff9307fa40bea20e57a7
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-03 00:00:50 +0000

    moved ValueAddons back again to /modules/

---
+++ sprachpaket_8/html/modules/Reviews/pnlang/deu/admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2001, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: admin.php 22139 2007-06-01 10:57:16Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Reviews
+ */
+
+define('_REVIEWS', 'Reviews');
+define('_REVIEWS_REVIEW', 'Review');
+define('_REVIEWS_REVIEWS', 'Reviews');
+
+define('_REVIEWS_GENERAL', 'Allgemeine Einstellungen');
---
+++ sprachpaket_8/html/modules/Top_List/pnlang/deu/user.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PostNuke Application Framework
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: user.php 22139 2007-06-01 10:57:16Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Top_List
+ */
+
+define('_ACTIVECAT','Aktivste Kategorien');
+define('_COMMENTEDSTORIES','Meist kommentierte Beiträge');
+define('_DOWNLOADEDFILES','Häufigste Downloads');
+define('_MOSTACTIVEAUTHORS','Aktivste Autoren:');
+define('_NEWSPUBLISHED','Beiträge veröffentlicht');
+define('_NEWSSENT','Beiträge eingereicht');
+define('_NEWSSUBMITTERS','Aktivste Beitrags-Schreiber');
+define('_NOTITLE','Kein Titel');
+define('_READREVIEWS','Meistgelesene Reviews');
+define('_READS','Gelesen');
+define('_READSECTION','Meistgelesene Sektions-Beiträge');
+define('_READSTORIES','Meistgelesene News');
+define('_TOPLISTVOTES','Stimmen');
+define('_TOPWELCOME','Willkommen bei der Top Liste');
+define('_VOTEDPOLLS','Erfolgreichste Umfragen');
