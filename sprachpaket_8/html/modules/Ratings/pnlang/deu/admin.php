2a2a5473f97a2691271142731c87d1acb67ee980
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-11-23 13:12:18 +0000

    minor updates (Core,Ratings)

--- sprachpaket_8/html/modules/Ratings/pnlang/deu/admin.php
+++ sprachpaket_8/html/modules/Ratings/pnlang/deu/admin.php
@@ -12,11 +12,13 @@
 
 define('_RATINGS','Ratings');
 define('_RATINGSDEFAULTSTYLE','Stil der Bewertung');
+define('_RATINGSNUMRATINGS','Anzahl der Bewertungen');
 define('_RATINGSOUTOFFIVE','Noten 1-5');
 define('_RATINGSOUTOFFIVESTARS','Sterne 1-5');
 define('_RATINGSOUTOFTEN','Noten 1-10');
 define('_RATINGSOUTOFTENSTARS','Sterne 1-10');
 define('_RATINGSPERCENTAGE','Prozentual');
+define('_RATINGSSCORE','Punkte');
 define('_RATINGSSECHIGH','Hoch (eine Abstimmung pro Benutzer)');
 define('_RATINGSSECLOW','Niedrig (mehrfache Abstimmung möglich)');
 define('_RATINGSSECMEDIUM','Mittel (eine Abstimmung pro Session)');
@@ -26,5 +28,5 @@ define('_RATINGSUSEAJAX', 'Soll Ajax für die Bewertung genutzt werden?');
 define('_RATINGSUSEFANCYCONTROLS', 'Die modernere Anzeige der Bewertung nutzen? (nur bei den Sternen)');
 define('_RATINGSVIEWITEMS', 'Alle Bewertungen anzeigen');
 define('_RATINGSVIEWLOG', 'Zeige log');
-define('_RATINGRATING', 'Rating');
-define('_RATINGITEMID', 'Item ID');
+define('_RATINGSRATING', 'Rating');
+define('_RATINGSITEMID', 'Item ID');
