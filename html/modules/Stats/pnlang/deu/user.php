56c141a6eda336007d4260da263c287565024581
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2007-12-04 19:38:08 +0000

    added stats-module

---
+++ html/modules/Stats/pnlang/deu/user.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id: pnadmin.php 19209 2006-06-05 13:57:35Z drak $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Stats
+ */
+
+// prose defines
+define('_STATS_PROSE_PAGEVIEWS', 'Bislang <strong>%total%</strong> Pagesviews seit <strong>%since%</strong> 
+<strong>%today%</strong> heute und <strong>%yesterday%</strong> gestern.');
+define('_STATS_PROSE_TRAFFIC', 'Der beste Tag war am %bestday% (<strong>%bestdayviews%</strong> Pageviews),
+ während %worstday% (<strong>%worstdayviews%</strong> Pageviews) kein wirklich guter Tag war.');
+define('_STATS_PROSE_DAYHOURVIEWS', 'Die meisten Besucher kommen am <strong>%bestweekday%</strong>, mit insgesamt <strong>%bestweekdayviews%
+</strong> Pageviews, während <strong>%worstweekday%</strong> iwenig frequentiert ist mit nur <strong>%worstweekdayviews%
+</strong> Pageviews. Unsere beste Stunde mit (mit <strong>%besthourviews%</strong> Pageviews ) beginnt um <strong>%besthour%:00</strong>.
+Wenig frequentiert ist mit nur <strong>%worsthour%:00</strong> (mit nur <strong>%worsthourviews%</strong> Pageviews).');
+
+// browsers
+define('_CAMINO', 'Camino');
+define('_CHIMERA', 'Chimera');
+define('_EPIPHANY', 'Epiphany');
+define('_FIREFOX', 'Firefox');
+define('_GALEON', 'Galeon');
+define('_KONQUEROR','Konqueror');
+define('_KMELEON', 'K-Meleon');
+define('_LYNX','Lynx');
+define('_MOZILLA', 'Mozilla');
+define('_MSIE','Internet Explorer');
+define('_NETSCAPE','Netscape');
+define('_OPERA','Opera');
+define('_SAFARI', 'Safari');
+define('_SEARCHENGINES','Suchmaschinen');
+define('_UNKNOWN', 'Unbekannte User-Agents');
+
+// module specific
+define('_REGUSERS','Registrierte Benutzer:');
+define('_ARTICLESSEC','Beiträge in Sektionen:');
+define('_COMMENTSPOSTED','Kommentare:');
+define('_LINKSCAT','Kategorien in Links:');
+define('_LINKSINLINKS','Links:');
+define('_NEWSWAITING','Beiträge zur Veröffentlichung:');
+define('_SACTIVETOPICS','Topics:');
+define('_SSPECIALSECT','Sektionen:');
+define('_STORIESPUBLISHED','Beiträge veröffentlicht:');
+
+// section headings
+define('_BROWSERS','Browser');
+define('_OPERATINGSYS','Betriebssysteme');
+define('_MISCSTATS','weitere Statistiken');
+define('_MODBYHOUR','Tagesstatistik');
+define('_MODBYMONTH','Monatsstatistik');
+define('_MODBYWEEK','Wochenstatistik');
+define('_STATS','Zugriffsstatistiken');
