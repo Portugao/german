0bea608a2770a8ae517b21cb3c14622ead85480d
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-01-06 13:19:34 +0000

    update [larsneo]

--- html/system/Profile/pnlang/deu/user.php
+++ html/system/Profile/pnlang/deu/user.php
@@ -11,7 +11,7 @@
  */
 
  // general
-define('_PROFILE',                'Profil');
+define('_PROFILE',                 'Profil');
 define('_PROFILE_ACCOUNTPANEL',    'Profil - Übersicht');
 
 // main template
@@ -20,7 +20,7 @@ define('_PROFILE_VIEW',            'Profil-Information anzeigen');
 define('_PROFILE_YOURINFO',        'Benutzerprofil');
 
 // view template
-define('_PROFILE_HOME',            'Zurück zur Site');
+define('_PROFILE_HOME',            'Zurück zur Webseite');
 define('_PROFILE_MAIN',            'Account');
 define('_PROFILE_MODIFY',          'Profil-Information modifizieren');
 
@@ -32,30 +32,30 @@ define('_TYPENEWPASSWORD',         'Neues Kennwort');
 define('_PASSWORDAGAIN',           'Kennwortwiederholung (zur Verifikation)');
 
 // status/error messages
-define('_PROFILE_EMPTYMAIL',      'Keine E-Mail-Adresse angegeben');
+define('_PROFILE_EMPTYMAIL',       'Keine E-Mail-Adresse angegeben');
 define('_PROFILE_INVALIDMAIL',     'Ungültige E-Mail-Adresse angegeben');
 define('_PROFILE_MAILEXISTS',      'E-Mail-Adresse wird bereits verwendet');
 define('_PROFILE_SAVEFAILED',      'Fehler! Die Profil-Informationen konnten nicht gespeichert werden.');
 define('_PROFILE_SAVEFIELDFAILED', 'Fehler! Das Feld %field% konnte nicht gespeichert werden.');
 define('_PROFILE_SAVEOK',          'Profil-Information gespeichert');
 define('_PROFILE_UNINDENTIFIED',   'Benutzer nicht bekannt');
-define('_PROFILE_WRONGPASS',       'Das Kennwort stimmt nicht überein');
+define('_PROFILE_WRONGPASS',       'Kennworte stimmen nicht überein');
 
 // Property strings
 define('_EXTRAINFO',               'Zusatzinformation');
 define('_SIGNATURE',               'Signatur');
 define('_TEST',                    'Testfeld DUD');
-define('_TIMEZONEOFFSET',         'Zeitzone');
-define('_UFAKEMAIL',              'Fake-E-Mail-Adresse');
-define('_UREALEMAIL',             'E-Mail-Adresse');
-define('_UREALNAME',              'Name');
-define('_YAIM',                   'AOL ID');
-define('_YICQ',                   'ICQ ID');
-define('_YINTERESTS',             'Interessen');
-define('_YMSNM',                  'MSN ID');
-define('_YLOCATION',              'Ort');
-define('_YOCCUPATION',            'Beruf');
-define('_YOURAVATAR',             'Avatar');
-define('_YOURHOMEPAGE',           'Homepage');
-define('_YYIM',                   'Yahoo ID');
-define('_YSKYPE',                 'Skype ID');
+define('_TIMEZONEOFFSET',          'Zeitzone');
+define('_UFAKEMAIL',               'angezeigte E-Mail-Adresse');
+define('_UREALEMAIL',              'E-Mail-Adresse');
+define('_UREALNAME',               'Name');
+define('_YAIM',                    'AOL ID');
+define('_YICQ',                    'ICQ ID');
+define('_YINTERESTS',              'Interessen');
+define('_YMSNM',                   'MSN ID');
+define('_YLOCATION',               'Ort');
+define('_YOCCUPATION',             'Beruf');
+define('_YOURAVATAR',              'Avatar');
+define('_YOURHOMEPAGE',            'Homepage');
+define('_YYIM',                    'Yahoo ID');
+define('_YSKYPE',                  'Skype ID');
