c44c77b61ae48d66572e71183790d3f2adae240c
larsneo <larsneo@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-01-12 14:21:41 +0000

    update [larsneo]

--- html/system/Users/pnlang/deu/online.php
+++ html/system/Users/pnlang/deu/online.php
@@ -8,7 +8,7 @@
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 
-define('_USERS_ONLINEBLOCK_CURRENTLYONLINE', 'Derzeit sind %g% %gs% und %m% %ms% online.');
+define('_USERS_ONLINEBLOCK_CURRENTLYONLINE', 'Aktuell sind %g% %gs% und %m% %ms% online.');
 define('_USERS_ONLINEBLOCK_PRIVATEMSGS0','m% neue Nachrichten.');
 define('_USERS_ONLINEBLOCK_PRIVATEMSGS1','%m% neue Nachricht.');
 define('_USERS_ONLINEBLOCK_PRIVATEMSGS','%m% neue Nachrichten.');
