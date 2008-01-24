871f5954db833eea889bb6c1225bd543c4f11212
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-01-24 20:18:06 +0000

    aktualisierte Displaynamen

--- sprachpaket_8/html/modules/Top_List/pnlang/deu/version.php
+++ sprachpaket_8/html/modules/Top_List/pnlang/deu/version.php
@@ -1,5 +1,5 @@
 <?php
 
 define('_TOPLIST_NAME', 'Top_List');
-define('_TOPLIST_DISPLAYNAME', 'Top List');
+define('_TOPLIST_DISPLAYNAME', 'Top-List');
 define('_TOPLIST_DESCRIPTION', 'Auflistung der Top-Beiträge');
