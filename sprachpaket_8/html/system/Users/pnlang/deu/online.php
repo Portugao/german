df9d3a00a89cc1939eb914ab3d965ea7aecdb161
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-02-03 12:01:04 +0000

    onlineblock (thx to Charlie)

--- sprachpaket_8/html/system/Users/pnlang/deu/online.php
+++ sprachpaket_8/html/system/Users/pnlang/deu/online.php
@@ -13,5 +13,7 @@ define('_USERS_ONLINEBLOCK_PRIVATEMSGS0','m% neue Nachrichten.');
 define('_USERS_ONLINEBLOCK_PRIVATEMSGS1','%m% neue Nachricht.');
 define('_USERS_ONLINEBLOCK_PRIVATEMSGS','%m% neue Nachrichten.');
 define('_USERS_ONLINEBLOCK_WHOSONLINE','Wer ist angemeldet');
-define('_USERS_ONLINEBLOCK_YOUAREANON','Noch nicht registriert oder angemeldet? <a href="%u%">Anmeldung</a>.');
+define('_USERS_ONLINEBLOCK_YOUAREANON','Noch nicht registriert oder angemeldet.');
+define('_USERS_ONLINEBLOCK_YOUCANREGISTERHERE','<a href="%u%">Hier Anmelden</a>.');
 define('_USERS_ONLINEBLOCK_YOUARELOGGED','Angemeldet als <strong>%uname%</strong>.');
+ 
