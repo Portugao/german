6829377837560d651f3e006d01e0562f57e49350
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-02-03 17:09:19 +0000

    german upgrade.php

---
+++ sprachpaket_8/html/install/pnlang/deu/upgrade.php
@@ -0,0 +1,45 @@
+<?php
+
+// language defines for the upgrade process
+
+// upgrade76.php
+define('_UPG_PROCEEDTOUPGRADE', 'Mit dem Upgrade fortfahren');
+define('_UPG_TITLE', 'PostNuke 0.764 - 0.8 Upgradeskript (für Version .764)');
+define('_UPG_SUBTITLE', 'PostNuke Upgradeskript (nur für Version .764)');
+define('_UPG_DESCRIPTION', 'Dieses Skript aktualisiert PostNuke 0.764 auf die aktuelle Version. Aktualisierungen von älteren Veröffentlichungen werden nicht unterstützt.');
+define('_UPG_BACKUPNOTICE', 'Unbedingt VOR dem Fortfahren eine Sicherung der Datenbanken anlegen');
+define('_UPG_BACKINGUPDB', 'Sichere Benutzerdatenbank... ');
+define('_UPG_DONE', 'erledigt');
+define('_UPG_USERDBSTRUCTUREUPGRADED', 'Datenbankstruktur -Users- aktualisiert.');
+define('_UPG_MODULEDBSTRUCTUREUPGRADED', 'Datenbankstruktur -Modules- aktualisiert.');
+define('_UPG_SUCCESSFUL', 'erfolgreich');
+define('_UPG_FAILED', 'FEHLGESCHLAGEN!');
+define('_UPG_BLOCKSTABLEUPGRADED', 'Tabelle -Blocks- aktualisiert');
+define('_UPG_RESETTHEMETOEXTRALITE', 'Theme auf ExtraLite zurücksetzen');
+define('_UPG_DUDMIGRATED', 'Benutzerattribute migriert');
+define('_UPG_SEARCHMODULEREINSTALLED', 'Suchmodul wiederinstalliert');
+define('_UPG_FOOTERINFO', 'Bitte die <a href="docs/upgrade.html">Upgradeanleitung</a> beachten. Weitere deutschsprachige Informationen gibt es auch unter <a href="http://support.pn-cms.de">support.pn-cms.de</a>');
+define('_UPG_TIMEHINT', 'Achtung: Je nachdem wie viele Benutzer in der Installation vorhanden sind, kann das Upgrade einige Zeit in Anspruch nehmen. Vorsichtshalber sollte ein Upgrade mit einer lokalen Sicherung getestet werden.');
+define('_UPG_STARTUPGRADE', 'Hier klicken, um den Upgradeprozess zu starten.');
+define('_UPG_USERSACTIVATED', 'Existierende Benutzer wurden aktiviert');
+
+// upgrade.php
+define('_UPG_MODULELISTREGENERATED', 'Modulliste abgeglichen');
+define('_UPG_INSTALLNEWMODULES', 'Installiere neue Systemmodule');
+define('_UPG_STARTNEWMODULEINSTALL', 'Starte Installation der neuen Systemmodule');
+define('_UPG_INITIALISED', 'initialisiert');
+define('_UPG_NOTINITIALISED', 'nicht initialisiert');
+define('_UPG_NONEWMODSNEEDINIT', 'Keine neuen zu initialisierenden Systemmodule');
+define('_UPG_INSTALLOFMODULESCOMPLETE', 'Installation der neuen Systemmodule fertiggestellt');
+define('_UPG_UPGRADEALLMODULES', 'Alle Module aktualisieren');
+define('_UPG_STARTINGUPGRADE', 'Upgrade starten');
+define('_UPG_UPGRADED', 'aktualisiert');
+define('_UPG_NOTUPGRADED', 'nicht aktualisiert');
+define('_UPG_NOMODSNEEDUPGRADE', 'Keine zu aktualisierenden Module');
+define('_UPG_FINISHEDUPGRADE', 'Upgrade abgeschlossen - ');
+define('_UPG_GOTOADMINPANEL', 'Administration für <a href="%s">%s</a> aufrufen');
+define('_UPG_FORTHENEXTSTEPSPLEASELOGIN', 'Für die nächsten Aktualisierungsschritte ist eine Anmeldung erforderlich. Bitte die Zugangsdaten für das Administrationskonto eingeben');
+define('_UPG_USERNAME', 'Benutzername');
+define('_UPG_PASSWORD', 'Kennwort');
+define('_UPG_SUBMIT', 'Fortfahren');
+define('_UPG_LOGINFAILED', 'Fehler bei der Anmeldung auf der Website');
