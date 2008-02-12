f9095bc2eb0893961706a8c9dd85299dd82d9b16
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-02-12 18:47:07 +0000

    update News,Quotes

---
+++ sprachpaket_8/html/modules/Quotes/pnlang/deu/init.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2004, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Quotes
+ */
+
+define('_QUOTES_IMPORTEDNAME', 'Importierte Zitate');
+define('_QUOTES_IMPORTEDDESCRIPTION', 'importierte Zitate aus PostNuke .7x');
