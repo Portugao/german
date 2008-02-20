a27ea5d5282df1cfe7af3871f4250c0b22404c23
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-02-20 15:37:39 +0000

    Tour-Modul (aber noch nicht vollständig übersetzt, kommt aber noch)

---
+++ sprachpaket_8/html/modules/Tour/pnlang/deu/user.php
@@ -0,0 +1,16 @@
+<?php
+
+define('_TOUR_TOUR', 'PostNuke Tour');
+define('_TOUR_HOME', 'Startseite');
+define('_TOUR_FTTOUR', 'Ersteinstieg');
+define('_TOUR_YOURDISTRIBUTION', 'Diese Distribution');
+define('_TOUR_NEWIN', 'Neu in PostNuke 0.8');
+define('_TOUR_BASICCONFIG', 'Basiskonfiguration');
+define('_TOUR_EXTENSIONTOURS', 'Erweiterungen');
+define('_TOUR_EXTENSIONS', 'Erweiterungen');
+define('_TOUR_MODULES', 'Module');
+define('_TOUR_BLOCKS', 'Blöcke');
+define('_TOUR_THEMES', 'Themes');
+define('_TOUR_PAGE', 'Seite %p%');
+define('_TOUR_START', 'Start');
+define('_TOUR_FINISH', 'Ende');
