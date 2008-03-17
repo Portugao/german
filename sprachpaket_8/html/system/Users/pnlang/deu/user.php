d0fa1bebcf4b2b459537427aa850a7436d138153
herr_vorragend <herr_vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2008-03-17 19:36:41 +0000

    typo

--- sprachpaket_8/html/system/Users/pnlang/deu/user.php
+++ sprachpaket_8/html/system/Users/pnlang/deu/user.php
@@ -91,7 +91,7 @@ define('_USERS_USERNAMENOSPACES','Fehler! Der Benutzername darf keine Leerzeiche
 define('_USERS_USERNAMETAKEN','Fehler! Der Benutzername wurde bereits registriert');
 define('_USERS_USERNAMERESERVED','Fehler! Der Benutzername ist nicht verfügbar');
 define('_USERS_USERNAMETOOLONG','Fehler! Der Benutzername ist zu lang - er muss kürzer als 25 Zeichen sein.');
-define('_USERS_YOUAREREGISTERED','Registrierung abgeschlossen. Die Zugangsinformationen werden an die hinterlegt E-Mail-Adresse verschickt.');
+define('_USERS_YOUAREREGISTERED','Registrierung abgeschlossen. Die Zugangsinformationen werden an die hinterlegte E-Mail-Adresse verschickt.');
 define('_USERS_YOURPASSMUSTBETHISLONG','Fehler! Das Kennwort muß mindestens %x% Zeichen lang sein');
 define('_USERS_ACCOUNTPOSSIBLYINACTIVE', 'Möglicherweise wurde die Registrierung auf dieser Seite noch nicht abgeschlossen. Bitte die Emails überprüfen und den Zugang ggf. aktivieren.');
 
