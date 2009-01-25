89ee94c270508bb9593c0425d8c3e951d4f73c2a
herr.vorragend <herr.vorragend@2e2000ab-e11c-0410-9ab3-cdf8a29eef7f>
2009-01-25 19:30:52 +0000

    synced with //core/changeset/25313

--- html/system/Settings/pnlang/deu/admin.php
+++ html/system/Settings/pnlang/deu/admin.php
@@ -13,14 +13,10 @@ define('_SETTINGS_ACTMULTILINGUAL','Multilingual-Umgebung aktivieren');
 define('_SETTINGS_ADMINEMAIL','Administrator E-Mail-Adresse');
 define('_SETTINGS_ADMINGRAPHIC','Icons in Administration');
 define('_SETTINGS_ALLOWUSERS','Benutzern die E-Mail-Adresse zeigen');
-define('_SETTINGS_ANONYMOUSSESSIONS', 'Sessions für Gäste benutzen');
-define('_SETTINGS_AUTHKEYUA', 'Authkey an UserAgent binden');
 
 define('_SETTINGS_BACKENDCONF','Backend Konfiguration');
 define('_SETTINGS_BASESITECONFIG', 'Basiskonfiguration');
-define('_SETTINGS_BASESITEINFO', 'Info');
-define('_SETTINGS_HTMLENTITIESBOX', 'HTML Entitäten');
-define('_SETTINGS_HTMLALLOWEDBOX', 'HTML Tags');
+define('_SETTINGS_BASESITEINFO', 'Informationen');
 define('_SETTINGS_MULTILINGUALBOX', 'Sprachsystem');
 define('_SETTINGS_BROWSERBOX', 'Browser');
 define('_SETTINGS_SERVERBOX', 'Server');
@@ -29,7 +25,6 @@ define('_SETTINGS_ERRORLOGGINGBOX', 'Fehler-Log');
 
 define('_SETTINGS_CENSORTEXT','Censor-Modul verwenden');
 
-define('_SETTINGS_DB', 'in Datenbank (empfohlen)');
 define('_SETTINGS_DETECTLANGUAGE','Spracherkennung');
 define('_SETTINGS_DYNKEYWORDS','Dynamische Meta-Keywords');
 
@@ -45,24 +40,10 @@ define('_SETTINGS_ERRORLOGTYPE','Typ des Error Logs');
 define('_SETTINGS_ERRORMAILTO','Fehler E-Mail schicken an');
 define('_SETTINGS_ERRORREPORTING','Error Reporting');
 
-define('_SETTINGS_FILE', 'in Datei');
-
-define('_SETTINGS_GC_PROBABILITY', 'Garbage collection probability');
-define('_SETTINGS_GC_RATIO', 'von 10000');
 define('_SETTINGS_GENERAL', 'Grundeinstellungen');
 define('_SETTINGS_GENSITEINFO','Allgemein');
 
-define('_SETTINGS_HTMLALLOWENTITIES','HTML Entitäten in Zeichen wandeln');
-define('_SETTINGS_HTMLOPT','HTML');
-define('_SETTINGS_HTMLTAGABOUT', 'Info "&lt;%t%&gt;"');
-define('_SETTINGS_HTMLTAGALLOWED','erlaubt');
-define('_SETTINGS_HTMLTAGALLOWEDWITHPARAMS','erlaubt mit Parametern');
-define('_SETTINGS_HTMLTAGNAME','Tag');
-define('_SETTINGS_HTMLTAGNOTALLOWED','nicht erlaubt');
-define('_SETTINGS_HTMLTAGUSAGE', 'Taginfo (von <a href="http://www.w3schools.com">W3Schools</a>)');
-define('_SETTINGS_HTMLWARNING','HINWEIS: Um das Risiko von Cross-Site-Scripting zu minimieren sollte - insbesondere bei Freigabe von Tags mit Parametern - der safehtml-Outputfilter im SecurityCenter aktiviert werden.');
 define('_SETTINGS_INVALIDEMAIL', 'Ungültige E-Mail-Adresse - bitte prüfen');
-define('_SETTINGS_KEYEXPIRY', 'Zeitlimit für Authkeys in Sekunden (default 0)');
 
 define('_SETTINGS_LANGDETECTBROWSER','Browser Sprache');
 define('_SETTINGS_LANGDETECTOFF','Keine Erkennung');
@@ -72,44 +53,20 @@ define('_SETTINGS_LOGERRORSONLY','Loggen von ausschliesslich errors');
 define('_SETTINGS_LOGFILEPERMODULE','Modulabhängig Logfile schreiben');
 define('_SETTINGS_LOGNOERRORS','Errors nicht loggen');
 
-define('_SETTINGS_MESSAGEMODULE', 'Modul für interne Kurzmitteilungen'); 
-define('_SETTINGS_MESSAGEMODULE_NONE', 'Kein internes Mitteilungssystem'); 
+define('_SETTINGS_MESSAGEMODULE', 'Modul für interne Kurzmitteilungen');
+define('_SETTINGS_MESSAGEMODULE_NONE', 'Kein internes Mitteilungssystem');
 define('_SETTINGS_METAKEYWORDS','Meta-Keywords');
 define('_SETTINGS_METATAGSETTINGS', 'Meta-Keywords');
 define('_SETTINGS_MISCSETTINGS', 'Verschiedenes');
 define('_SETTINGS_MLOPT', 'Multilingual');
 define('_SETTINGS_MLSETTINGS', 'Multilingual Einstellungen');
 
-define('_SETTINGS_OTHERSEC', 'Weitere Sicherheitsoptionen');
-define('_SETTINGS_COOKIES', 'Cookie-Einstellungen');
-
 define('_SETTINGS_PASSBYMAIL','(Benutzern nach Registrierung das Kennwort via E-Mail schicken)');
 define('_SETTINGS_PHPSYSTEMLOG', 'Das PHP System Log (definiert in php.ini) nutzen');
-define('_SETTINGS_PROFILEMODULE', 'Modul für die Benutzerprofilverwaltung'); 
-define('_SETTINGS_PROFILEMODULE_NONE', 'Keine Benutzerprofile'); 
+define('_SETTINGS_PROFILEMODULE', 'Modul für die Benutzerprofilverwaltung');
+define('_SETTINGS_PROFILEMODULE_NONE', 'Keine Benutzerprofile');
 
-define('_SETTINGS_SAFEHTMLWARNING', 'Achtung! Der <a href="%url%">SafeHTML Output-Filter</a> überschreibt einige Einstellungen von HTML-Tags (z.B. object und embed)');
-define('_SETTINGS_SECHIGH','Hoch (Benutzer müssen sich bei jedem Besuch anmelden)');
-define('_SETTINGS_SECINACTIVELENGTH','Benutzer werden inaktiv nach');
-define('_SETTINGS_SECINACTIVETXT','Minuten ohne Aktivität');
-define('_SETTINGS_SECLEVEL','Sicherheitsstufe');
-define('_SETTINGS_SECLOW','Niedrig (Benutzer bleiben dauerhaft angemeldet)');
-define('_SETTINGS_SECMEDIUM','Mittel (Benutzer bleiben für eine definierte Tageszahl angemeldet)');
-define('_SETTINGS_SECMEDLENGTH','Bei mittlerer Sicherheitsstufe bleiben Benutzer angemeldet für');
-define('_SETTINGS_SECMEDTXT','Tagen (bei aktivierter Erinnerung)');
-define('_SETTINGS_SECOPT','Sicherheit');
 define('_SETTINGS_SELLANGUAGE','Default Sprache für diese Webseite');
-define('_SETTINGS_SESSIONIPCHECK', 'IP Check für Session (eventuell Probleme mit AOL und Proxy-Benutzern)');
-define('_SETTINGS_SESSIONNAME', 'Session-Cookie Name');
-define('_SETTINGS_SESSIONNAMEWARNING', 'Nach Änderung müssen sich alle Benutzer neu anmelden');
-define('_SETTINGS_SESSIONPATHNOTWRITABLE', 'Pfad für Sessions nicht beschreibbar!');
-define('_SETTINGS_SESSIONREGENERATE', 'Session ID bei An-/Abmeldung neu erzeugen');
-define('_SETTINGS_SESSIONRANDREGENERATE', 'Periodisch Session ID neu erzeugen');
-define('_SETTINGS_SESSIONREGENERATEFREQ', 'Wahrscheinlichkeit');
-define('_SETTINGS_SESSIONREGENERATEFREQ_RATIO', 'von 100');
-define('_SETTINGS_SESSIONS', 'Sessions');
-define('_SETTINGS_SESSIONSAVEPATH', 'Pfad für Speicherung der Session-Dateien');
-define('_SETTINGS_SESSIONSAVEPATHTIP', 'Achtung: Wenn die Option \'Session-Speicherung\' auf  \'Datei\' gesetzt wird, muss der Pfad in im Feld weiter oben angegeben werden. Der Pfad muss beschreibbar sein.');
 define('_SETTINGS_SHORTURLS', 'Short URLs');
 define('_SETTINGS_SHORTURLSDIRECTORY', 'Verzeichnis');
 define('_SETTINGS_SHORTURLSENABLE', 'Short URLs aktivieren');
@@ -118,23 +75,19 @@ define('_SETTINGS_SHORTURLSHTACCESS', 'Short URLs benötigen eine .htaccess Datei
 define('_SETTINGS_SHORTURLSNOTE', "Für Short URLs muß die Apache-Option modrewrite aktiv sein. Falls PHP im CGI-Modus läuft, sollte die Option 'cgi.fix_pathinfo = 0' in der php.ini aktiv sein, damit Short URLs einwandfrei funktionieren.");
 define('_SETTINGS_SHORTURLSSTRIPENTRYPOINT', 'Einstiegspunkt aus Verzeichnisbasierenden URLs entfernen');
 define('_SETTINGS_SHORTURLSTYPE', 'Typ der Short URLs');
-define('_SETTINGS_SHORTURLS_DEFAULTMODULE', 'Zu benutzendes Modul, wenn Permalink keinen Modulnamen beinhaltet'); 
+define('_SETTINGS_SHORTURLS_DEFAULTMODULE', 'Zu benutzendes Modul, wenn Permalink keinen Modulnamen beinhaltet');
 define('_SETTINGS_SHORTURLS_EXTENSION', 'Erweiterung für Dateibasierende URLs');
-define('_SETTINGS_SHORTURLS_NODEFAULTMODULE', 'Keine Standardmodul'); 
+define('_SETTINGS_SHORTURLS_NODEFAULTMODULE', 'Keine Standardmodul');
 define('_SETTINGS_SHORTURLS_SEPARATOR', 'Trennzeichen im Permalink Titel');
 define('_SETTINGS_SHOWALLERRORS','notices, warnings und errors anzeigen');
 define('_SETTINGS_SHOWERRORSONLY','real errors anzeigen');
 define('_SETTINGS_SHOWNOERRORS','errors nicht anzeigen');
-define('_SETTINGS_SIGNCOOKIES', 'Cookies signieren?');
-define('_SETTINGS_SIGNINGKEY', 'Signatur Key');
 define('_SETTINGS_SINGLELOGFILE','In das Global Logfile schreiben');
 define('_SETTINGS_SITECONFIG','Webseite Konfiguration');
 define('_SETTINGS_SITELOGO','Logo für RSS und Druckerthemes');
 define('_SETTINGS_SITENAME', 'Name');
 define('_SETTINGS_SITEOFF', 'Seite deaktivieren');
 define('_SETTINGS_SITEOFFREASON', 'Grund für Deaktivierung');
-define('_SETTINGS_SITESECUREURL','Secure Hostname');
-define('_SETTINGS_SITESECUREURL1','Nur angeben wenn ein anderer Hostname für HTTPS verwendet wird');
 define('_SETTINGS_SITESLOGAN','Beschreibung');
 define('_SETTINGS_STARTARGS', 'optional: Argumente für die Modulfunktion');
 define('_SETTINGS_STARTARGSDESCR', 'kommasepariert (name=value,name2=value2)');
@@ -142,22 +95,11 @@ define('_SETTINGS_STARTFUNC','Modulfunktion');
 define('_SETTINGS_STARTMODULE', 'Startseite');
 define('_SETTINGS_STARTPAGE','Startmodul');
 define('_SETTINGS_STARTPAGEDESCR','(Modul, auf das index.php verweist)');
-define('_SETTINGS_STARTPAGE_NOMODULE', 'Kein Startmodul (statische Startseite)'); 
+define('_SETTINGS_STARTPAGE_NOMODULE', 'Kein Startmodul (statische Startseite)');
 define('_SETTINGS_STARTTYPE', 'Funktionstyp');
 
 define('_SETTINGS_TIMEZONEOFFSET', 'Timezone für unregistrierte Benutzer');
 define('_SETTINGS_TIMEZONESERVER', 'Timezone des Servers');
 
-define('_SETTINGS_UPDATESETTINGS', 'Automatische Updateüberprüfung'); 
-define('_SETTINGS_UPDATECHECK', 'Nach Updates suchen'); 
-define('_SETTINGS_UPDATEFREQUENCY', 'Häufigkeit'); 
-define('_SETTINGS_DAILY', 'Täglich'); 
-define('_SETTINGS_WEEKLY', 'Wöchentlich'); 
-define('_SETTINGS_MONTHLY', 'Monatlich'); 
-
 define('_SETTINGS_USECOMPRESSION','GZip-Kompression aktivieren');
 define('_SETTINGS_USERCONFIGURATION', 'Benutzerkonfiguration');
-
-define('_SETTINGS_WHERETOSAVE', 'Session-Speicherung');
-define('_SETTINGS_WHERETOSAVETXT1', 'Warnung! Änderungen führen zur Abmeldung.');
-
