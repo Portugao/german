3991f0ba6c954f9defb4e2295bc69731498ec552
Herr.Vorragend <carsten@zikula.org>
2011-08-05 20:53:35 +0200

    Removed modules directory, because we have now an own topic branch for this

--- translation/modules/Topics/trunk/lang/deu/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('_TOPICS_NAME', 'Topics');
-define('_TOPICS_DISPLAYNAME', 'Topics');
-define('_TOPICS_DESCRIPTION', 'Anzeige der Themen');
--- translation/modules/photoshare/tags/photoshare-4.2.0/templates/gallery/lang/deu/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ------------------------------------------------------------------------------------
-// Translation for Photoshare 'Gallery' template
-// Language:              Danish
-// Translation by:        Jorn Lind-Nielsen
-// ------------------------------------------------------------------------------------
-define('_PTMP_GALLERY_TITLE', 'Gallerie');
-define('_PTMP_GALLERY_EMPTYALBUM', 'Gallerie ist leer');
-?>
